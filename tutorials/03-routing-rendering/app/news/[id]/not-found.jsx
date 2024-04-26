12d7b7680221ad582472d5ea62fcdfbcce3f495e
Cyro <coolronitchs@gmail.com>
2024-04-27 01:34:50 +0530

    first commit

---
+++ tutorials/03-routing-rendering/app/news/[id]/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div id="error">
+      <h1>Not Found</h1>
+      <p>The requested page couldn't be found</p>
+    </div>
+  );
+}
---
+++ tutorials/next-level-food/components/meals/image-picker.js
@@ -0,0 +1,5 @@
+import classes from "./image-picker.module.css";
+
+export default function ImagePicker({ label }) {
+  return <div className={classes.picker}></div>;
+}
