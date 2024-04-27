7ff43d8359d3ac1613aa64683f3490d6756e9028
Cyro <coolronitchs@gmail.com>
2024-04-27 11:34:25 +0530

    folder refreshed

--- tutorials/03-routing-rendering/app/not-found.jsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div id="error">
-      <h1>Not Found</h1>
-      <p>The requested page couldn't be found</p>
-    </div>
-  );
-}
--- tutorials/next-level-food/components/meals/image-picker.js
+++
@@ -1,5 +0,0 @@
-import classes from "./image-picker.module.css";
-
-export default function ImagePicker({ label }) {
-  return <div className={classes.picker}></div>;
-}
